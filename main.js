54a211d38946048afc80fe321453c20e38e1c8cc
mnmt <mnmtsnghh@gmail.com>
2023-02-18 16:07:32 +0530

    Completed Navbar styling and responsiveness

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
+const primaryHeader = document.querySelector(".primary-header");
 
 navToggle.addEventListener("click", () => {
     navToggle.setAttribute("aria-expanded", !primaryNav.hasAttribute("data-visible"));
     primaryNav.toggleAttribute("data-visible");
+    primaryHeader.toggleAttribute("data-overlay");
 });
