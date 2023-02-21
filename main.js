602ac3a771148dc036f80affcff9055f4956eacc
mnmt <mnmtsnghh@gmail.com>
2023-02-21 20:41:44 +0530

    Worked on Carousel section

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
+const slider = new A11YSlider(document.querySelector(".slider"), {
+  adaptiveHeight: true,
+  dots: false,
+});
+
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
 const primaryHeader = document.querySelector(".primary-header");
 
 navToggle.addEventListener("click", () => {
-    navToggle.setAttribute("aria-expanded", !primaryNav.hasAttribute("data-visible"));
-    primaryNav.toggleAttribute("data-visible");
-    primaryHeader.toggleAttribute("data-overlay");
+  navToggle.setAttribute(
+    "aria-expanded",
+    !primaryNav.hasAttribute("data-visible")
+  );
+  primaryNav.toggleAttribute("data-visible");
+  primaryHeader.toggleAttribute("data-overlay");
 });
